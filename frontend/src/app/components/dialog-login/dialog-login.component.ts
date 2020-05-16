624e2085d16e18c98f9e7083bf9ed3e08eac2881
Fra <deliafra0@gmail.com>
2020-05-16 02:20:33 +0200

    feat(Projects connection): Frontend and Backend connection with REST API

--- frontend/src/app/components/dialog-login/dialog-login.component.ts
+++ frontend/src/app/components/dialog-login/dialog-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dialog-login',
@@ -14,20 +15,24 @@ export class DialogLoginComponent implements OnInit {
   password: string;
 
   constructor(private dialogRef: MatDialogRef<DialogLoginComponent>,
-    private spinner: NgxSpinnerService) { }
+    private spinner: NgxSpinnerService,
+    public authService: AuthService) { }
 
   ngOnInit() {
-
   }
 
   login() {
-    this.spinner.show();
+    // this.spinner.show();
  
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 5000);
-    this.dialogRef.close();
+    // setTimeout(() => {
+    //   /** spinner ends after 5 seconds */
+    //   this.spinner.hide();
+    // }, 5000);
+    this.authService.login(this.username).subscribe(item => {
+      console.log(item);
+      
+    })
+    // this.dialogRef.close(true);
   }
 
   exit() {
